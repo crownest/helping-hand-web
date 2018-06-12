9ce21f573125c4697ed1dab583d6a8193c3be183
fdurmaz <muhammedfurkandurmaz@gmail.com>
2018-06-12 17:24:59 +0300

    Create base service to GET token

--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -1,31 +1,43 @@
 // Packages
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 // Constants
-import { language } from 'constants/languageConstants';
+import {language} from 'constants/languageConstants';
 
 // Objects
 import Header from 'objects/Header/index'
 
+//Services
+import {authLogin} from "../../services/coreServices";
 
 export default class Index extends Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    document.title = language.trans("Index") + " | Helping Hand";
+        document.title = language.trans("Index") + " | Helping Hand";
 
-    this.state = {
-      "title": "Helping Hand" 
-    };
-  }
+        this.state = {
+            "title": "Helping Hand"
+        };
+    }
 
-  render() {
-    const { title } = this.state;
+    componentDidMount() {
+        const data = {
+            email: "crownest@unicrow.com",
+            password: "helping"
+        }
+        authLogin(data, (response) => {
+            console.log(response);
+        });
+    }
 
-    return (
-      <div id="index-page">
-        <Header title={title} />
-      </div>
-    );
-  }
+    render() {
+        const {title} = this.state;
+
+        return (
+            <div id="index-page">
+                <Header title={title}/>
+            </div>
+        );
+    }
 }
