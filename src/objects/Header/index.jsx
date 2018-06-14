18c8ab569108fb0f72e9276318179c6e25b53a01
fdurmaz <muhammedfurkandurmaz@gmail.com>
2018-06-14 11:14:41 +0300

    Use React-Modal to make the Register and Login page appear and disappear when clicking on Signup/Login.

--- src/objects/Header/index.jsx
+++ src/objects/Header/index.jsx
@@ -1,16 +1,54 @@
 // Packages
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import Modal from 'react-modal';
+import ReactDOM from 'react-dom';
+
+Modal.setAppElement('#root');
 
 
 export default class Header extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            showModal: false,
+            seeModal: false
+        };
+
+        this.handleOpenModal = this.handleOpenModal.bind(this);
+        this.handleCloseModal = this.handleCloseModal.bind(this);
+
+        this.seeOpenModal = this.seeOpenModal.bind(this);
+        this.seeCloseModal = this.seeCloseModal.bind(this);
     }
+
+
+    handleOpenModal() {
+        this.setState({showModal: true});
+    }
+
+    handleCloseModal() {
+        this.setState({showModal: false});
+    }
+
+    seeOpenModal() {
+        this.setState({seeModal: true});
+    }
+
+    seeCloseModal() {
+        this.setState({seeModal: false});
+    }
+
     handleClick = e => {
         e.preventDefault();
         this.props.rightNowHandler();
     }
+
+    onClick(event) {
+        this.handleCloseModal;
+        this.seeOpenModal;
+    }
+
     render() {
         return (
             <header>
@@ -34,8 +72,10 @@ export default class Header extends Component {
                         className="material-icons">menu</i></Link>
                     <div className="head-nav">
                         <ul className="login-signup">
-                            <li><Link to="#register" className="register-btn">Sign up</Link></li>
-                            <li><Link to="#login" className="login-btn">Login</Link></li>
+                            <li><Link to="#register" className="register-btn" onClick={this.handleOpenModal}>Sign
+                                up</Link>
+                            </li>
+                            <li><Link to="#login" className="login-btn" onClick={this.seeOpenModal}>Login</Link></li>
                         </ul>
 
                         <div className="user-nav hide">
@@ -50,8 +90,107 @@ export default class Header extends Component {
 
                     </div>
                 </div>
+                <Modal
+                    isOpen={this.state.showModal}
+                    contentLabel="Register"
+                    onRequestClose={this.handleCloseModal}>
+                    <div id="register" className="modal">
+
+                        <div className="modal-content">
+                            <a href="#" className="close" onClick={this.handleCloseModal}><i
+                                className="material-icons">close</i></a>
+                            <div className="modal-img"></div>
+                            <div className="form-area">
+                                <div className="form-title">
+                                    <h4>Register.</h4>
+                                    <p>Complete all the fields in order to complete your registration</p>
+                                </div>
+                                <div className="form-item">
+                                    <input type="text" name="name" placeholder="Name"/>
+                                </div>
+                                <div className="form-item">
+                                    <input type="text" name="surname" placeholder="Surname"/>
+                                </div>
+                                <div className="form-item">
+                                    <input type="email" name="email" placeholder="E-Mail"/>
+                                </div>
+                                <div className="form-item">
+                                    <input type="password" name="password" placeholder="Password"/>
+                                </div>
+                                <div className="form-item">
+                                    <input type="password" name="confirm-password" placeholder="Confirm Password"/>
+                                </div>
+                                <div className="form-item">
+                                    <div className="select-box gender-select">
+                                        <select>
+                                            <option>Gender</option>
+                                            <option>Erkek</option>
+                                            <option>Kadın</option>
+                                        </select>
+                                    </div>
+                                    <div className="select-box country-select">
+                                        <select>
+                                            <option>Country</option>
+                                            <option>Türkiye</option>
+                                            <option>Amerika</option>
+                                            <option>Almanya</option>
+                                        </select>
+                                    </div>
+
+                                </div>
+                                <div className="form-item">
+                                    <div className="checkbox">
+                                        <input type="checkbox" id="privacy" name="privacy"/>
+                                        <label htmlFor="privacy">Kullanım koşullarını okudum ve kabul ediyorum.</label>
+                                    </div>
+                                </div>
+                                <div className="form-item">
+                                    <button type="submit" className="btn full">Continue</button>
+                                </div>
+                                <div className="form-item">
+                                    <p className="form-info">Do you already have an account? <a href="#" onClick={(event) => { this.handleCloseModal(); this.seeOpenModal();}}>Log
+                                        in here</a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Modal>
+
+                <Modal
+                    isOpen={this.state.seeModal}
+                    contentLabel="Login"
+                    onRequestClose={this.seeCloseModal}>
+                    <div id="login" className="modal login-modal">
+                        <div className="modal-content">
+                            <a href="#" className="close" onClick={this.seeCloseModal}><i
+                                className="material-icons">close</i></a>
+                            <div className="form-area">
+                                <div className="form-title">
+                                    <h4>Welcome.</h4>
+                                    <p>Fill in the required fields to log in.</p>
+                                </div>
+                                <div className="form-item">
+                                    <input type="email" name="email" placeholder="E-Mail"/>
+                                </div>
+                                <div className="form-item">
+                                    <input type="password" name="password" placeholder="Password"/>
+                                    <a href="#" className="forgot-password">Forgot Password</a>
+                                </div>
+                                <div className="form-item">
+                                    <button type="submit" className="btn full">Login</button>
+                                </div>
+                                <div className="form-item">
+                                    <p className="form-info">If you're not a member yet, please <a href="#" onClick={(event) => { this.seeCloseModal(); this.handleOpenModal();}}>Register</a>
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="modal-img"></div>
+                        </div>
+                    </div>
+                </Modal>
             </header>
 
-        );
+        )
     }
 }
