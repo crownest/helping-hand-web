4e2cb74e772c2a429ae4d601e795f8c8f332c844
fdurmaz <muhammedfurkandurmaz@gmail.com>
2018-06-21 10:28:27 +0300

    Create Functionality of the Right Navigation bar to show needs when clicked on menu icon

--- src/components/RightNav/index.jsx
+++ src/components/RightNav/index.jsx
@@ -2,14 +2,45 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
+// Constants
+import {HTTP_200_OK, HTTP_400_BAD_REQUEST} from "../../constants/serviceConstants";
+
+// Services
+import {listNeed} from "../../services/needServices";
+import {isAuthentication} from "../../services/baseServices";
 
 export default class RightNav extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            needs: [],
+        };
+
+        this.setNeeds = this.setNeeds.bind(this);
+    }
+
+    componentWillMount() {
+        if (isAuthentication()) {
+            listNeed((response) => {
+                if (response) {
+                    if (response.statusCode === HTTP_200_OK) {
+                        this.setNeeds(response.body);
+                    }
+                }
+            });
+        }
+    }
+
+    setNeeds = (needs) => {
+        this.setState({
+            needs: needs
+        });
     }
 
     render() {
         const isRightActiveClass = this.props.isOpen ? 'open' : '';
+        const {needs} = this.state;
         return (
             <div className={"right-menu " + isRightActiveClass}>
                 <div className="search-area">
@@ -17,76 +48,19 @@ export default class RightNav extends Component {
                     <button type="submit"><i className="material-icons">search</i></button>
                 </div>
                 <ul>
-                    <li>
-                        <Link to="#">
-                            <div className="img"><img src="/images/temp/img01.jpg" alt=""/></div>
-                            <div className="text">
-                                <h6><img src="/images/icon/durum-mini-1.png" alt=""/><span>Headline</span></h6>
-                                <p>Yardıma ihtiyacı olanların bilgilerinin kısaltması burada yer alacak.</p>
-                                <span className="count">1326 people wanted to help.</span>
-                            </div>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="#">
-                            <div className="img"><img src="/images/temp/img01.jpg" alt=""/></div>
-                            <div className="text">
-                                <h6><img src="/images/icon/durum-mini-2.png" alt=""/><span>Headline</span></h6>
-                                <p>Yardıma ihtiyacı olanların bilgilerinin kısaltması burada yer alacak.</p>
-                                <span className="count">1326 people wanted to help.</span>
-                            </div>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="#">
-                            <div className="img"><img src="/images/temp/img01.jpg" alt=""/></div>
-                            <div className="text">
-                                <h6><img src="/images/icon/durum-mini-3.png" alt=""/><span>Headline</span></h6>
-                                <p>Yardıma ihtiyacı olanların bilgilerinin kısaltması burada yer alacak.</p>
-                                <span className="count">1326 people wanted to help.</span>
-                            </div>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="#">
-                            <div className="img"><img src="/images/temp/img01.jpg" alt=""/></div>
-                            <div className="text">
-                                <h6><img src="/images/icon/durum-mini-4.png" alt=""/><span>Headline</span></h6>
-                                <p>Yardıma ihtiyacı olanların bilgilerinin kısaltması burada yer alacak.</p>
-                                <span className="count">1326 people wanted to help.</span>
-                            </div>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="#">
-                            <div className="img"><img src="/images/temp/img01.jpg" alt=""/></div>
-                            <div className="text">
-                                <h6><img src="/images/icon/durum-mini-5.png" alt=""/><span>Headline</span></h6>
-                                <p>Yardıma ihtiyacı olanların bilgilerinin kısaltması burada yer alacak.</p>
-                                <span className="count">1326 people wanted to help.</span>
-                            </div>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="#">
-                            <div className="img"><img src="/images/temp/img01.jpg" alt=""/></div>
-                            <div className="text">
-                                <h6><img src="/images/icon/durum-mini-4.png" alt=""/><span>Headline</span></h6>
-                                <p>Yardıma ihtiyacı olanların bilgilerinin kısaltması burada yer alacak.</p>
-                                <span className="count">1326 people wanted to help.</span>
-                            </div>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="#">
-                            <div className="img"><img src="/images/temp/img01.jpg" alt=""/></div>
-                            <div className="text">
-                                <h6><img src="/images/icon/durum-mini-5.png" alt=""/><span>Headline</span></h6>
-                                <p>Yardıma ihtiyacı olanların bilgilerinin kısaltması burada yer alacak.</p>
-                                <span className="count">1326 people wanted to help.</span>
-                            </div>
-                        </Link>
-                    </li>
+                    {this.state.needs.map(need =>
+                        <li>
+                            <Link to="#">
+                                <div className="img"><img src="/images/temp/img01.jpg" alt=""/></div>
+                                <div className="text">
+                                    <h6><img src="/images/icon/durum-mini-1.png" alt=""/><span>{need.title}</span>
+                                    </h6>
+                                    <p>{need.description}</p>
+                                    <span className="count">21 people wanted to help.</span>
+                                </div>
+                            </Link>
+                        </li>
+                    )}
                 </ul>
                 <Link to="#" className="btn full">Show other need</Link>
             </div>
