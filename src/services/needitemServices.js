d4ddd321d985f7338dbcc706a6e7e5b7d7650018
fdurmaz <muhammedfurkandurmaz@gmail.com>
2018-06-25 18:05:25 +0300

    Add parentheses

--- src/services/needitemServices.js
+++ src/services/needitemServices.js
@@ -46,52 +46,53 @@ export function createNeedItem(data, onComplete) {
         .end(function (error, response) {
             onComplete(response);
         });
+}
 
-    export function retrieveNeedItem(need_id, onComplete) {
-        var auth_informations = getAuthInformations();
+export function retrieveNeedItem(need_id, onComplete) {
+    var auth_informations = getAuthInformations();
 
-        return request
-            .get(api_needitems_url + need_id + '/')
-            .set("Authorization", "TOKEN " + auth_informations.auth_token)
-            .type("application/json")
-            .accept("application/json")
-            .end((error, response) => {
-                onComplete(response);
-            });
-    }
+    return request
+        .get(api_needitems_url + need_id + '/')
+        .set("Authorization", "TOKEN " + auth_informations.auth_token)
+        .type("application/json")
+        .accept("application/json")
+        .end((error, response) => {
+            onComplete(response);
+        });
+}
 
-    export function updateNeedItem(needitem_id, data, onComplete) {
-        /*
-          data = {
-            name: "Acer Laptop",
-            remaining: 3,
-            is_fixed: false,
-            need: 1
-          }
-        */
+export function updateNeedItem(needitem_id, data, onComplete) {
+    /*
+      data = {
+        name: "Acer Laptop",
+        remaining: 3,
+        is_fixed: false,
+        need: 1
+      }
+    */
 
-        var auth_informations = getAuthInformations();
+    var auth_informations = getAuthInformations();
 
-        return request
-            .put(api_needitems_url + needitem_id + '/')
-            .set("Authorization", "TOKEN " + auth_informations.auth_token)
-            .type("application/json")
-            .accept("application/json")
-            .send(data)
-            .end(function (error, response) {
-                onComplete(response);
-            });
-    }
+    return request
+        .put(api_needitems_url + needitem_id + '/')
+        .set("Authorization", "TOKEN " + auth_informations.auth_token)
+        .type("application/json")
+        .accept("application/json")
+        .send(data)
+        .end(function (error, response) {
+            onComplete(response);
+        });
+}
 
-    export function deleteNeedItem(needitem_id, onComplete) {
-        var auth_informations = getAuthInformations();
+export function deleteNeedItem(needitem_id, onComplete) {
+    var auth_informations = getAuthInformations();
 
-        return request
-            .del(api_needitems_url + needitem_id + '/')
-            .set("Authorization", "TOKEN " + auth_informations.auth_token)
-            .type("application/json")
-            .accept("application/json")
-            .end((error, response) => {
-                onComplete(response);
-            });
-    }
+    return request
+        .del(api_needitems_url + needitem_id + '/')
+        .set("Authorization", "TOKEN " + auth_informations.auth_token)
+        .type("application/json")
+        .accept("application/json")
+        .end((error, response) => {
+            onComplete(response);
+        });
+}
