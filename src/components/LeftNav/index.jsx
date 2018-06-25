5d3c0d47e76f43b2aacc96d19ae79165d2c3b33f
fdurmaz <muhammedfurkandurmaz@gmail.com>
2018-06-25 10:56:10 +0300

    - Updated Need component to send categories when creating a new need - Updated LeftNav component to show all fixed and !fixed Needs - Updated Support component to subtract the input from the remaining number

--- src/components/LeftNav/index.jsx
+++ src/components/LeftNav/index.jsx
@@ -2,25 +2,58 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
+// Services
+import {listNeed} from "../../services/needServices";
+import {HTTP_200_OK} from "../../constants/serviceConstants";
+import {isAuthentication} from "../../services/baseServices";
 
 export default class LeftNav extends Component {
     constructor() {
         super();
 
         this.state = {
-            isMenuOpen: false
+            isMenuOpen: false,
+            needs: []
         };
+
+        this.setNeeds = this.setNeeds.bind(this);
     }
+
+    componentWillMount() {
+        if (isAuthentication()) {
+            listNeed((response) => {
+                if (response) {
+                    if (response.statusCode === HTTP_200_OK) {
+                        this.setNeeds(response.body);
+                    }
+                }
+            });
+        }
+    }
+
+    setNeeds = (needs) => {
+        this.setState({
+            needs: needs
+        });
+    }
+
 
     render() {
         const isActiveClass = this.state.isMenuOpen ? 'open' : '';
+        const {needs} = this.state;
+        let isFixedCount = 0;
+        needs.forEach((item) => {
+            if (item.is_fixed) {
+                isFixedCount += 1;
+            }
+        });
         return (
             <div className={"left-menu " + isActiveClass}>
                 <Link to="#" className="open-btn"
                       onClick={() => this.setState({isMenuOpen: !this.state.isMenuOpen})}>Open/Close</Link>
-                <span className="check"><img src="/images/icon/check.png" alt=""/>180 needs are solved</span>
+                <span className="check"><img src="/images/icon/check.png" alt=""/>{isFixedCount} needs are solved</span>
                 <Link to="/need" className="add"><img src="/images/icon/add.png" alt=""/>Add a need</Link>
-                <span className="wait"><img src="/images/icon/refresh.png" alt=""/>210 needs are waiting</span>
+                <span className="wait"><img src="/images/icon/refresh.png" alt=""/>{needs.length - isFixedCount} needs are waiting</span>
             </div>
         );
     }
